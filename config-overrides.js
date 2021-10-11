d7c9e8ad91fd3f47d93785871c109f96eecf8c4a
bpgould <bgould@protonmail.com>
2021-10-11 17:37:45 -0400

    current before reverting

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,4 @@
-module.exports = function override(config) {
+export default function override(config) {
     let loaders = config.module.rules[1].oneOf;
     loaders.splice(loaders.length - 1, 0, 
         {
